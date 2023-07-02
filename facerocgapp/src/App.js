8121ceced7cf36f242c8239e45624a832a4aef33
derek <derek987987@hotmail.com>
2023-07-03 02:08:03 +0800

    can input photo link and can detect face, can change different models to detect something else

---
+++ facerocgapp/src/App.js
@@ -0,0 +1,201 @@
+import React, {Component} from 'react';
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import Clarifai from "clarifai";
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = 'c17c36740ffa41a1bc7fdf2de3724531';
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID = 'clarifai';       
+const APP_ID = 'main';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '45fb9a671625463fa646c3523a3087d5';
+
+const particlesoptions = {
+  background: {
+      color: {
+          value: "#0d47a1",
+      },
+  },
+  fpsLimit: 120,
+  interactivity: {
+      events: {
+          onClick: {
+              enable: true,
+              mode: "push",
+          },
+          onHover: {
+              enable: true,
+              mode: "repulse",
+          },
+          resize: true,
+      },
+      modes: {
+          push: {
+              quantity: 4,
+          },
+          repulse: {
+              distance: 200,
+              duration: 0.4,
+          },
+      },
+  },
+  particles: {
+      color: {
+          value: "#ffffff",
+      },
+      links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+      },
+      collisions: {
+          enable: true,
+      },
+      move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+              default: "bounce",
+          },
+          random: false,
+          speed: 3,
+          straight: false,
+      },
+      number: {
+          density: {
+              enable: true,
+              area: 800,
+          },
+          value: 80,
+      },
+      opacity: {
+          value: 0.5,
+      },
+      shape: {
+          type: "circle",
+      },
+      size: {
+          value: { min: 1, max: 5 },
+      },
+  },
+  detectRetina: true,
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.particlesInit = this.particlesInit.bind(this);
+    this.particlesLoaded = this.particlesLoaded.bind(this);
+    this.state = {
+        input:'',
+        imageUrl:'',
+        box:{}
+    }
+  }
+
+  calculateFaceLocation = (result) => {
+    //console.log(result.outputs[0].data.regions[0].region_info.bounding_box);
+    const clarifaiFace = result.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const height = Number(image.height);
+    const width = Number(image.width);
+    //console.log(width, height);
+    return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({input: event.target.value})
+  }
+
+  onButtonSubmit = () => {
+    console.log("onButtonSubmit this.state.input", this.state.input);
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": this.state.input
+                    }
+                }
+            }
+        ]
+    });
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    //console.log('click');
+    this.setState({imageUrl: this.state.input})
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+            //console.log(result.outputs[0].data.regions[0].region_info.bounding_box);
+            this.displayFaceBox(this.calculateFaceLocation(result));
+        })
+        .catch(error => console.log('error', error));
+  }
+
+  async particlesInit(engine) {
+    console.log(engine);
+    await loadFull(engine);
+  }
+
+  async particlesLoaded(container) {
+    console.log(container);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className='particles'
+            id="tsparticles"
+            init={this.particlesInit}
+            loaded={this.particlesLoaded}
+            options={particlesoptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition box={this.state.box} imageUrl = {this.state.imageUrl} />
+  
+      </div>
+    );
+  } 
+}
+
+export default App;
---
+++ facerocgapp/src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'hello'}
+            </div>
+            <div className='white f1'>
+                {'Derek'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
