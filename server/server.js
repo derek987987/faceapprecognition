dd384bdff852811bf3f8af3c30c328a3572ddb90
derek <derek987987@hotmail.com>
2023-07-12 21:42:56 +0800

    need body parser to read json, add a sample user database to check, add signin endpoint

--- server/server.js
+++ server/server.js
@@ -2,9 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const database = {
+    users:[
+        {
+            id:'123',
+            name:'derek',
+            email:'derek@gmail.com',
+            password:'cookies',
+            entries:0,
+            joined: new Date()
+        },
+        {
+            id:'124',
+            name:'john',
+            email:'john@gmail.com',
+            password:'boxes',
+            entries:0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/',(req,res)=>{
     res.send('this is working');
+})
+
+app.post('/signin',(req,res)=>{
+    
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.send('this is signing');
+    }else{
+        res.send('there is error signin');
+    }
 })
 
 app.use(express.static(__dirname + '/public'))
